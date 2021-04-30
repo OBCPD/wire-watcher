8e9f8f1f8ad0b7e4fecce1d4925057a4ba912177
AlexandreHiroyuki <alexandrehiroyuki@hotmail.com>
2021-04-30 09:23:57 -0300

    delete unused code

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 
 import Navigator from "./navigation/navigator";
@@ -7,7 +6,6 @@ import Navigator from "./navigation/navigator";
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      {/* <StatusBar barStyle='dark-content' translucent={true} /> */}
       <Navigator />
     </View>
   )
