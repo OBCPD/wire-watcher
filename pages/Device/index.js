00a504f201b9e3d0c8ba3bd3321199a091cdf5ba
Amorim33 <aluamorim@hotmail.com>
2021-05-01 11:19:35 -0300

    Adding debug

--- pages/Device/index.js
+++ pages/Device/index.js
@@ -1,36 +1,65 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { LineChart, YAxis, Grid } from 'react-native-svg-charts'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import React, { useState } from "react";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { LineChart, YAxis, Grid } from "react-native-svg-charts";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import styles from './styles'
+import styles from "./styles";
 
 export default function Device(props) {
   const device = props.navigation.getParam("device");
+
+  const [heatArray, setHeatArray] = useState(device.heat);
+  const [heat, setHeat] = useState("0");
+  const inputHeatHandler = (value) => {
+    setHeat(value);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{device.name}</Text>
       <Text style={styles.grapicTitle}>Temperature History</Text>
-        <View style={{ height: 200, flexDirection: 'row' }}>
-          <YAxis
-            data={device.heat}
-            contentInset={{ top: 20, bottom: 20 }}
-            svg={{
-                fill: 'grey',
-                fontSize: 10,
-            }}
-            numberOfTicks={10}
-            formatLabel={(value) => `${value}ºC`}
-          />
-          <LineChart
-            style={{ flex: 1, marginLeft: 16 }}
-            data={device.heat}
-            svg={{ stroke: '#0016a8' }}
-            contentInset={{ top: 20, bottom: 20 }}
-          >
-            <Grid />
-          </LineChart>
-        </View>
+      <View style={{ height: 200, flexDirection: "row" }}>
+        <YAxis
+          data={heatArray}
+          contentInset={{ top: 20, bottom: 20 }}
+          svg={{
+            fill: "grey",
+            fontSize: 10,
+          }}
+          numberOfTicks={10}
+          formatLabel={(value) => `${value}ºC`}
+        />
+        <LineChart
+          style={{ flex: 1, marginLeft: 16 }}
+          data={heatArray}
+          svg={{ stroke: "#0016a8" }}
+          contentInset={{ top: 20, bottom: 20 }}
+        >
+          <Grid />
+        </LineChart>
+      </View>
+      <Text style={styles.formLabel}>(Debug/Test)</Text>
+      <Text style={styles.formLabel}>
+        Heat input simulating the entering of the sensors:
+      </Text>
+      <TextInput
+        keyboardType="numeric"
+        style={styles.formInput}
+        placeholder="input here"
+        onChangeText={(text) => inputHeatHandler(text)}
+        value={heat}
+      />
+      <TouchableOpacity
+        style={styles.formButton}
+        onPress={() => {
+          let aux = [heatArray, ...parseInt(heat, 10)];
+          setHeatArray(aux);
+          console.log(heatArray);
+        }}
+      >
+        <MaterialCommunityIcons name="arrow-collapse-up" size={20} />
+        <Text style={styles.formButtonText}> Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- pages/Device/styles.js
+++ pages/Device/styles.js
@@ -1,21 +1,49 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 12,
-        paddingTop: 12,
-    },
+  container: {
+    flex: 1,
+    paddingHorizontal: 12,
+    paddingTop: 12,
+  },
 
-    title: {
-        textAlign: 'center',
-        textTransform: 'capitalize',
-        fontSize: 32,
-        marginBottom: 12,
-    },
+  title: {
+    textAlign: "center",
+    textTransform: "capitalize",
+    fontSize: 32,
+    marginBottom: 12,
+  },
 
-    grapicTitle: {
-        fontSize: 16,
-        paddingLeft: 12,
-    },
-})
+  grapicTitle: {
+    fontSize: 16,
+    paddingLeft: 12,
+  },
+  formLabel: {
+    paddingLeft: 4,
+    marginBottom: 2,
+  },
+
+  formInput: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "#0016a8",
+    padding: 8,
+    marginBottom: 12,
+  },
+
+  formButton: {
+    padding: 8,
+    borderRadius: 4,
+    backgroundColor: "#0016a83a",
+
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  formButtonText: {
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
